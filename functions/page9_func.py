99eadf97634109c05b7f87e34bbc83b4b6d058a7
Muench-Shi <3311386889@qq.com>
2022-02-22 17:09:12 +0800

    2022-2-22/18

--- functions/page9_func.py
+++ functions/page9_func.py
@@ -10,32 +10,32 @@ from sumy.summarizers.lex_rank import LexRankSummarizer
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-# import spacy
-# from spacy import displacy
-# nlp = spacy.load("en_core_web_sm")
-#
-# from gensim.summarization import summarize
-#
-# # --------Page9:summarizer and entity checker总结和实体检查-----------------
-# # Function for Sumy Summarization
-# def sumy_summarizer(docx):
-#     parser = PlaintextParser.from_string(docx, Tokenizer("chinese"))
-#     lex_summarizer = LexRankSummarizer()
-#     summary = lex_summarizer(parser.document,3)
-#     summary_list = [str(sentence) for sentence in summary]
-#     result = ' '.join(summary_list)
-#     return result
-#
-#
-# # Fetch Text From Url
-# @st.cache
-# def get_text(raw_url):
-#     page = urlopen(raw_url)
-#     soup = BeautifulSoup(page)
-#     fetched_text = ' '.join(map(lambda p:p.text,soup.find_all('p')))
-#     return fetched_text
-#
-#
-# def analyze_text(text):
-#     return nlp(text)
-# # -----------------------------------------------------------------------
+import spacy
+from spacy import displacy
+nlp = spacy.load("en_core_web_sm")
+
+from gensim.summarization import summarize
+
+# --------Page9:summarizer and entity checker总结和实体检查-----------------
+# Function for Sumy Summarization
+def sumy_summarizer(docx):
+    parser = PlaintextParser.from_string(docx, Tokenizer("chinese"))
+    lex_summarizer = LexRankSummarizer()
+    summary = lex_summarizer(parser.document,3)
+    summary_list = [str(sentence) for sentence in summary]
+    result = ' '.join(summary_list)
+    return result
+
+
+# Fetch Text From Url
+@st.cache
+def get_text(raw_url):
+    page = urlopen(raw_url)
+    soup = BeautifulSoup(page)
+    fetched_text = ' '.join(map(lambda p:p.text,soup.find_all('p')))
+    return fetched_text
+
+
+def analyze_text(text):
+    return nlp(text)
+# -----------------------------------------------------------------------
